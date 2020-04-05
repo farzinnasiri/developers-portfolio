9af65639a269dc0c3dad7a4b096733f34c616d7b
farzinnasiri <farzinnasiri890@gmail.com>
2020-04-05 15:41:46 +0430

    about component added

---
+++ src/components/About.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container-fluid about">
+    <div class="row">
+      <div class="col-sm-6 about-image">
+        <g-image
+          class="profile-image"
+          src="@/assets/profile_photo.jpg"
+          alt="Profile Photo"
+          quality="100"
+          width="450"
+        />
+      </div>
+      <div class="col-sm-6 about-text">
+        <div class="card text-black">
+          <div class="card-body">
+            <h3 class="card-title">About</h3>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn text-white border-light">Outline</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data: () => ({}),
+};
+</script>
+<style>
+.profile-image {
+  border-radius: 0%;
+  box-shadow: -30px -25px 0px 10px rgb(51, 50, 50);
+  padding: 0px;
+  margin-top: 2rem;
+}
+.about {
+  padding-top: 3rem;
+  display: block;
+}
+.about-image {
+  text-align: center;
+}
+.about-text {
+
+}
+.card{
+    border: none;
+}
+@media screen and (max-width: 480px) {
+  .profile-image {
+    width: 300px;
+  }
+}
+</style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,27 +1,33 @@
 <template>
   <div class="intro">
-    <header class="group vcenter intro__header">
-      <h1>
-        Hi, I'm <strong>{{ personal.name }}</strong>
-      </h1>
-      <p class="roles">
-        I am a
-        <vue-typer
-          class="typing-list"
-          :text="personal.roles"
-          :repeat="Infinity"
-          :shuffle="false"
-          initial-action="typing"
-          :pre-type-delay="70"
-          :type-delay="60"
-          :pre-erase-delay="2000"
-          :erase-delay="40"
-          erase-style="backspace"
-          :erase-on-complete="false"
-          caret-animation="blink"
-        />
-      </p>
-    </header>
+    <div class="container-fluid vcenter ">
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h1>
+            Hi, I'm <strong>{{ personal.name }}</strong>
+          </h1>
+          <p class="roles">
+            I'm a
+            <ClientOnly>
+              <vue-typer
+                class="typing-list"
+                :text="personal.roles"
+                :repeat="Infinity"
+                :shuffle="false"
+                initial-action="typing"
+                :pre-type-delay="1000"
+                :type-delay="120"
+                :pre-erase-delay="2000"
+                :erase-delay="1000"
+                erase-style="select-all"
+                :erase-on-complete="false"
+                caret-animation="blink"
+              />
+            </ClientOnly>
+          </p>
+        </div>
+      </div>
+    </div>
 
     <section id="view-portfolio">
       <a class="portfolio-button button" href="#works" align="middle">
@@ -63,8 +69,9 @@ export default {
   position: relative;
   box-sizing: border-box;
   width: 100%;
-  padding: 17rem 5rem 17rem 5rem;
-  background: #1e88e5;
+  padding-top: 10rem;
+  padding-bottom: 10rem;
+  background: #111111;
   color: white;
   text-align: center;
   font-size: 1rem;
@@ -93,8 +100,29 @@ export default {
 }
 .portfolio-button:hover {
   text-decoration: none;
+  color: green;
 }
 .roles {
-  font-size: 30px;
+  font-size: 25px;
+}
+.vue-typer .custom.char.typed {
+  color: #76ff03;
+  font-weight: bold;
+  font-family: "Inconsolata";
+}
+.vue-typer .custom.char.selected {
+  font-weight: bold;
+  font-family: "Inconsolata";
+}
+
+.vue-typer .custom.caret {
+  width: 0.5rem;
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+.profile-image {
+  border-radius: 5%;
+  padding: 1rem 1rem 1rem 1rem;
+  margin-bottom: 2rem;
 }
 </style>
